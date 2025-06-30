e3e7bbe84baec12dae16c7e1e3452a8a87b7fa17
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2025-06-30 20:05:25 +0000

    Pastas Controller, Entity e Repository

--- delivery-api/src/main/java/com/deliverytech/delivery/repository/ClienteRepository.java
+++ delivery-api/src/main/java/com/deliverytech/delivery/repository/ClienteRepository.java
@@ -1,23 +1,7 @@
-package com.deliverytech.delivery_api.repository;
+package com.deliverytech.delivery.repository;
 
-import com.deliverytech.entity.Cliente;
+import br.com.delivery.api.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import java.util.List;
-import java.util.Optional;
 
-@Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-
-    // Buscar cliente por email
-    Optional<Cliente> findByEmail(String email);
-
-    // Buscar apenas clientes ativos
-    List<Cliente> findByAtivoTrue();
-
-    // Busca por nome (parcial, ignorando case)
-    List<Cliente> findByNomeContainingIgnoreCase(String nome);
-
-    // Verificar se email já existe
-    boolean existsByEmail(String email);
-}
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {}
+ 
